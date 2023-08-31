43f1b2f7d0b2df204d08d660a339d0b8f8dce7d4
moondrop0816 <moondrop0816@gmail.com>
2023-09-01 00:25:58 +0900

    fix: 언어 수정

--- site.config.js
+++ site.config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // CONFIG configration (required)
   link: "https://harriet-log.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
-  lang: ["ko-kr", "en-US"], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-kr", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
