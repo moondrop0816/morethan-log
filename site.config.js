13597dba34b4f847260bc890c11fd5d531aaa31b
moondrop0816 <moondrop0816@gmail.com>
2023-09-01 00:53:40 +0900

    fix : 서비스 수정

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: ``,
-      href: "",
+      name: `blog`,
+      href: "https://harriet-log.vercel.app",
     },
   ],
   // blog setting (required)
@@ -24,7 +24,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://harriet-log.vercel.app",
-  since: 2023, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   lang: "ko-kr", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
