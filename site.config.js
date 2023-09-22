b3385382ed53c6bd7b54e5b610b577bd01cb861a
moondrop0816 <moondrop0816@gmail.com>
2023-09-22 12:11:42 +0900

    fix : 프로젝트 링크 및 블로그 링크 변경

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `blog`,
-      href: "https://harriet-log.vercel.app",
+      href: "https://harrylog.vercel.app",
     },
   ],
   // blog setting (required)
@@ -23,8 +23,8 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://harriet-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://harrylog.vercel.app",
+  since: 2021, // If leave this empty, current year will be used.
   lang: "ko-kr", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
